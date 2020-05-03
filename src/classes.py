e7443ad99bdcacf4fc2bdd313a6fb24052e07118
Dieter Brehm <inkering@gmail.com>
2020-05-02 23:07:04 -0400

    add cuuid to assignment class, write data_importer functions

--- src/classes.py
+++ src/classes.py
@@ -15,7 +15,7 @@ DAY_MAP = ['S', 'U', 'M', 'T', 'W', 'R', 'F']
 
 class Course:
     """ Holds all the information about the class. """
-    
+
     def __init__(self, name, start, end, days):
         self.uuid = name
         self.start = start
@@ -27,7 +27,7 @@ class Course:
 
 class NINJA(Course):
     """ Holds all the information pertaining to NINJA hours. """
-    
+
     def __init__(self, nname, **ckwargs):
         self.ninja = nname
         super().__init__(**ckwargs)
@@ -35,9 +35,10 @@ class NINJA(Course):
 
 class Assignment:
     """ The data model containing the information for homework assignments. """
-    
-    def __init__(self, cname, duration, desc, duedate):
+
+    def __init__(self, cname, cuuid, duration, desc, duedate):
         self.cname = cname
+        self.cuuid = cuuid
         self.duration = duration * NUM_BLOCKS
         self.desc = desc
         self.duedate = DAY_MAP.index(duedate)
@@ -45,7 +46,7 @@ class Assignment:
 
 class TODO(Assignment):
     """ The model for when to complete what homework assignment. """
-    
+
     def __init__(self, start, day, hw):
         self.start = start
         self.end = start + hw.duration
--- src/data_importer.py
+++ src/data_importer.py
@@ -1,7 +1,28 @@
 import numpy as np
 import pandas as pd
 import random
-from classes import *
+from classes import Course, NINJA, Assignment, TODO
+
+
+def day_parse(row):
+    days = []
+    # handler for if S//U column doesn't exist?
+    if row['S'] == 1:
+        days.append("S")
+    elif row['U'] == 1:
+        days.append("U")
+    elif row['M'] == 1:
+        days.append("M")
+    elif row['T'] == 1:
+        days.append("T")
+    elif row['W'] == 1:
+        days.append("W")
+    elif row['TH'] == 1:
+        days.append("TH")
+    elif row['F'] == 1:
+        days.append("F")
+
+    return days
 
 
 def load_classes(file_name):
@@ -13,6 +34,14 @@ def load_classes(file_name):
     data = pd.read_csv(path)
 
     class_list = []
+
+    for index, row in data.iterrows():
+        days = day_parse(row)
+        course = Course(uuid=row['CUUID'],
+                        start=row['start-block'], end=row['end-block'],
+                        days=days)
+
+        class_list.append(course)
 
     return class_list
 
@@ -26,6 +55,17 @@ def load_ninja_hrs(file_name):
     data = pd.read_csv(path)
 
     ninja_list = []
+
+    for index, row in data.iterrows():
+        days = day_parse(row)
+        # todo: change column name to ninja-name or something
+        a_ninja = NINJA(nname=row['Class'],
+                        uuid=row['CUUID'],
+                        start=row['start-block'], end=row['end-block'],
+                        days=days)
+
+        ninja_list.append(a_ninja)
+
     return ninja_list
 
 
@@ -38,4 +78,16 @@ def load_homework(file_name):
     data = pd.read_csv(path)
 
     homework_list = []
+
+    for index, row in data.iterrows():
+        days = day_parse(row)
+        # todo: change column name to ninja-name or something
+        a_assignment = NINJA(cname=row['class'],
+                             cuuid=row['CUUID'],
+                             desc=row['assignment'],
+                             duedate=row[due]
+                             # TODO: add durations column)
+
+        home_list.append(a_assignment)
+
     return homework_list
